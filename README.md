63906037e16fb5384f0caca93c792d6af186d96c
Gabriel Manea <gabriel.maneaa@gmail.com>
2025-04-19 16:24:20 +0300

    [V1] Push working code and description

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
-# log-parser
+# Description
+**parser.py** is the main file in which constants, methods for initialization and a class are defined.
+It uses multiprocessing to parse multiple files at once, but the output file is shared among all processes.
+A lock was used to keep things simple and avoid errors due to file being already in use by another process created. 
+An in depth analysis should be done to verify if the processing time was actually decreased or a bottleneck was introduced. This
+could've been avoided by having an output file for each input, but then we should've had a method for aggregation for results.
+
+# Installation
+All packages used are shipped with python3, so theoretically there is nothing to be installed
+
+# Defaults
+```
+    output_file = 'current_directory/output'
+    input_logs_folder = 'current_directory/logs'
+    regex_used_for_log_file = '^logs-[0-9]+.log$'
+```
+
+# Usage
+```
+    python -m parser [-l, --logs <path_to_logs_folder>]
+    # or
+    python parser.py [-l, --logs <path_to_logs_folder>]
+```
+
+# Result
+An WARNING message will be present in 'output_file' if the process took longer than 5 minutes and 
+an ERROR message if it took longer than 10 minutes.
+Also, as INFO are logged duplicate processes (which started twice) - in this case, they are simply
+discarded / deleted from map and no analysis is done for them & processes which ended but never started.
+They were considered anomalies which are unlikely to happen.
+
+# To do
+Time analysis, change to a @classmethod for **parse** function to avoid
+creating an object for each process, clean-up of output file
---
+++ parser.py
@@ -0,0 +1,115 @@
+from datetime import datetime
+import os
+import logging
+import multiprocessing
+from argparse import ArgumentParser
+import re
+import glob
+
+""" Constants """
+MIN5 = 5 * 60
+MIN10 = 10 * 60
+FORMAT="%(asctime)s;%(logfile)s;%(levelname)s;%(message)s"
+
+""" Initialize logger """
+logging.basicConfig(filename=os.path.join(os.getcwd(), "output"), format=FORMAT, datefmt="%Y-%m-%d %H:%M:%S")
+logger = logging.getLogger("Parser")
+logger.setLevel(logging.INFO)
+
+""" Multiprocess target function """
+def process_function(path: str = None, multiprocess: bool = False, lock: multiprocessing.Lock = None):
+    Parser(path, multiprocess, lock)
+
+""" Argument parser """
+def argument_parser() -> ArgumentParser:
+    cwd = os.getcwd()
+    parser = ArgumentParser()
+    parser.add_argument("-l", "--logs", default=os.path.join(cwd, "logs"), type=str, help="Logs directory path", dest="logs")
+    return parser
+
+""" Parser class """
+class Parser():
+    """
+    Parser class:
+    :argument path str - mandatory - path of log file to be parsed
+    :argument multiprocess bool - whether to use multiprocessing or not
+    :argument lock multiprocessing.Lock - lock object to use for multiprocessing
+    """
+    _path: str
+    lock: multiprocessing.Lock
+    multiprocess: bool
+
+    def __init__(self, path: str = None, multiprocess: bool = False, lock: multiprocessing.Lock = None):
+        try:
+            self.path = path
+            self.lock = lock
+            self.multiprocess = multiprocess
+        except FileNotFoundError as e:
+            print("File path does not exist or none")
+            raise e
+        except Exception as e:
+            print("Another exception occurred")
+            raise e
+        self.parse(path, multiprocess, lock)
+
+    @property
+    def path(self) -> str:
+        return self.path
+    @path.setter
+    def path(self, path: str) -> None:
+        if path is None: raise FileNotFoundError()
+        if os.path.exists(path) and os.path.isfile(path): _path = path
+        else: raise FileNotFoundError()
+
+    def parse(self, path: str, multiprocess: bool, lock: multiprocessing.Lock) -> None:
+        with (open(path, "r") as file):
+
+            status = {}
+
+            for index, line in enumerate(file):
+                if line.endswith("\n"): line = line.rstrip("\n")
+                if line.endswith("\r"): line = line.rstrip("\r")
+
+                extra = {"logfile": os.path.basename(path)}
+                timestamp, description, action, pid= line.split(",")
+                if multiprocess: lock.acquire()
+
+                try:
+                    if action.lstrip() == "START":
+                        if status.get(pid, None) is not None:
+                            del status[pid]
+                            raise Exception(f"Duplicate detected: 2 applications running under same PID - {pid}")
+                        status[pid] = (timestamp, description, action)
+                    elif action.lstrip() == "END":
+                        if status.get(pid, None) is None:
+                            raise Exception(f"PID ended, but never started - {pid}")
+                        time_start = status.get(pid)[0]
+                        time_end = timestamp
+                        difference_seconds = int((datetime.strptime(time_end, "%H:%M:%S") \
+                                        - datetime.strptime(time_start, "%H:%M:%S")).total_seconds())
+
+                        if MIN5 <= difference_seconds < MIN10:
+                            logger.warning(f"Job with PID - {pid} took longer than 5 minutes", extra=extra)
+                        elif difference_seconds >= MIN10:
+                            logger.error(f"Job with PID - {pid} took longer than 10 minutes", extra=extra)
+                except Exception as e:
+                    logger.info(f"Exception parsing line with number {index} - {e}", extra=extra)
+                finally:
+                    if multiprocess: lock.release()
+
+if __name__ == "__main__":
+    ns = argument_parser().parse_args()
+    lock = multiprocessing.Lock()
+    regex = re.compile(r"^logs-[0-9]+.log$", flags=re.IGNORECASE)
+    files = glob.glob(ns.logs + "/**")
+
+    process_list = []
+
+    for file in files:
+        match = re.match(regex, os.path.basename(file))
+        if match is not None:
+            process = multiprocessing.Process(target=process_function, args=(file, True, lock))
+            process_list.append(process)
+            process.start()
+
+    for process in process_list: process.join()
